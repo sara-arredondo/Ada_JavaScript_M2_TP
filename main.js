a0408caee24093f62a460369b3ed4571045003b8
Sara Arredondo Giraldo <sara.arredondo.giraldo@gmail.com>
2024-12-13 20:41:42 -0500

    se adiciona funcionalidad de input color y boton de descargar

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-// ------------------ definicion de variables para filtros-------------------
+window.onload = () => {
 
+// ------------------ definicion de variables y query selector-------------------
+
+const $ = element => document.querySelector(element)
+
+const $colorValue = $("color-value")
 const $inputBrillo = $("#brillo")
 const $inputOpacidad = $("#opacidad")
 const $inputConstraste = $("#contraste")
@@ -10,7 +15,28 @@ const $inputHue = $("#hue")
 const $inputSaturacion = $("#saturacion")
 const $inputNegativo = $("#negativo")
 const $buttonReset = $("#reset")
+const $inputColor = $("#color")
+const $imgContainer = $("#img-container")
+const $buttonDownload = $(".button-download-style")
+const $memeContainer = $("#meme-container")
+const $topText =$("#top-text")
+const $bottomText =$("#bottom-text")
 
+//---------------------------- input seleccionar color --------------------------------- -------------------
+
+$inputColor.addEventListener("input", () => {
+    $imgContainer.style.backgroundColor = $inputColor.value
+    //$colorValue.innerText = $inputColor.value
+    console.log ($inputColor.value)
+})
+   
+// ------------------------codigo para descargar imagen---------------------------
+
+$buttonDownload.addEventListener("click", () => {
+    domtoimage.toBlob($memeContainer).then((blob) => {
+        saveAs(blob, "mi-primer-meme.png");
+      });
+})
 
 // --------------------  inicio codigo para funcion de reestablecer valores----------------------------
 
@@ -33,3 +59,7 @@ $buttonReset.addEventListener("click", reestablecerValores)
 
 
 // --------------------  fin codigo para funcion de reestablecer valores----------------------------
+
+
+
+}
